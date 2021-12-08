9617ad7da659c9363e469766b53b945c470d6fbb
unknown <shimondriham523@gmail.com>
2021-12-08 10:59:43 +0200

    make it better

--- public/js_admin/newAdmin.js
+++ public/js_admin/newAdmin.js
@@ -1,45 +1,72 @@
+import { doApiMethod } from "../services/apiService.js";
 window.onload = () => {   
-    init();
-}
-
-const init = () => {
-    declareEvents();
+  declareEvents();
 }
 
 const declareEvents = () => {
   let id_form = document.querySelector("#id_form");
+
   id_form.addEventListener("submit", (e) => {  
     e.preventDefault();
-    let bodyObj = {
-      Name: document.querySelector("#id_name").value,
+    doApi();
+  })
+}
+const doApi = () => {
+ let url = "https://toys1234.herokuapp.com/users";
+    let body = {
+      name: document.querySelector("#id_name").value,
       email: document.querySelector("#id_email").value,
       password: document.querySelector("#id_password").value,
     }
-    let url = "https://toys1234.herokuapp.com/users";
-    doApiWithMethod(url, bodyObj, "POST");
-  })
-}
+    try{
+        let data = await doApiMethod(url, "POST" ,body);
+        if(data._id){
+          alert("You sign up success , now log in")
+          window.location.href = "login.html";
+        }
+        else if(data.msg){
+          alert("User already in system, try login")
+        }
+        else{
+          alert("there problem ,come back later")
+        }
+      }
+      catch(err){
+        console.log(err);
+        alert("There big problem ! come back later")
+      }
+    
+    }
 
-const doApiWithMethod = async (_url, _bodyPayload, _method) => {
-  try {
-    let resp = await fetch(_url, {
-      method: _method,
-      body: JSON.stringify(_bodyPayload),
-    })
-    let data = await resp.json();
-    if (data._id) {
-      alert("user added succefuly!")
-      console.log("qq")
-      window.location.href = "login.html";
-    }
-    else{
-      console.log(data);
-      alert(data[0].message)
-    }
-    console.log(data);
-  }
-  catch(err){
-    console.log(err);
-    alert("There problem, come back later")
-  }
-}
+
+
+
+
+
+
+//     doApiWithMethod(url, bodyObj, "POST");
+// }
+
+// const doApiWithMethod = async (_url, _body, _method) => {
+//   try {
+//     let resp = await fetch(_url, {
+//       method: _method,
+//       body: JSON.stringify(_bodyPayload),
+//     })
+//     let data = await resp.json();
+//     if (data._id) {
+//      
+//       console.log("qq")
+//       window.location.href = "login.html";
+//     }
+//     else{
+//       console.log(data);
+//       alert(data[0].message)
+//     }
+//     console.log(data);
+//   }
+//   catch(err){
+//     console.log(err);
+//     alert("There problem, come back later")
+//   }
+// }
