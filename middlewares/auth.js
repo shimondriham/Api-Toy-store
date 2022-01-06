393c6332d8bc3ae7abf6de5c7cc283b98fab5dd6
shimondriham <95618903+shimondriham@users.noreply.github.com>
2022-01-07 01:20:51 +0200

    Add files via upload

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,6 @@
 const jwt = require("jsonwebtoken");
+const {config} = require("../config/secret");
+
 
 exports.auth = (req, res, next) => {
   let token = req.header("x-api-key");
@@ -6,7 +8,7 @@ exports.auth = (req, res, next) => {
     return res.status(401).json({ msg: "You must send token" })
   }
   try {
-    let decodToken = jwt.verify(token, "shimonSecret");
+    let decodToken = jwt.verify(token, config.TokenSecret);
     req.userTokenData = decodToken;
     next();
   }
